c1893ecd9d8da8e1210204db05b667a23351605a
acascoc098 <acascoc098@g.educaand.es>
2023-10-10 11:00:27 +0200

    Creación del load de la clase Personas.

--- src/main/java/com/iesvdc/acceso/modelos/Personas.java
+++ src/main/java/com/iesvdc/acceso/modelos/Personas.java
@@ -14,13 +14,14 @@ public class Personas {
     private List<String> apellidos;
     private Direcciones direcc;
 
-    public void load(String nom_muj_filename,String nom_homb_filename,String apellido_filename){
+    public void load(String nom_muj_filename,String nom_homb_filename,String apellido_filename, String locals_filename, String calles_filename){
 
         try {
             this.personas = new ArrayList<Persona>();
             this.nom_muj = Files.readAllLines(Paths.get(nom_muj_filename), StandardCharsets.UTF_8);
             this.nom_hom = Files.readAllLines(Paths.get(nom_homb_filename), StandardCharsets.UTF_8);
             this.apellidos = Files.readAllLines(Paths.get(apellido_filename), StandardCharsets.UTF_8);
+            this.direcc.load(locals_filename,calles_filename);
         }catch (Exception e){
             e.printStackTrace();
         }
