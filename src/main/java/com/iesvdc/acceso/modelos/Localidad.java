d15c91e96148c217a6dff738a00d20d3bc2224e2
acascoc098 <acascoc098@g.educaand.es>
2023-10-20 08:52:52 +0200

    Añadimos LocalidadesTest y cambiamos código en las clases Localidad y Localidades.

--- src/main/java/com/iesvdc/acceso/modelos/Localidad.java
+++ src/main/java/com/iesvdc/acceso/modelos/Localidad.java
@@ -1,41 +1,41 @@
 package com.iesvdc.acceso.modelos;
 
 import java.util.Objects;
-
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
-
+ 
 @XmlRootElement(name = "localidad")
-@XmlAccessorType(XmlAccessType.FIELD)
+@XmlAccessorType (XmlAccessType.FIELD)
 public class Localidad {
-    //private String municipio;
-    private String poblacion;
-    private int cp;
+    private String ciudad;
+    private Integer cp;
     private String provincia;
 
 
-    public Localidad(){}
+    public Localidad() {
+    }
 
-    public Localidad(String poblacion, int cp, String provincia){
-        this.poblacion = poblacion;
+    public Localidad(String ciudad, Integer cp, String provincia) {
+        this.ciudad = ciudad;
         this.cp = cp;
         this.provincia = provincia;
     }
 
-    public String getpoblacion() {
-        return this.poblacion;
+    public String getCiudad() {
+        return this.ciudad;
     }
 
-    public void setpoblacion(String poblacion) {
-        this.poblacion = poblacion;
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
     }
 
-    public int getCp() {
+    public Integer getCp() {
         return this.cp;
     }
 
-    public void setCp(int cp) {
+    public void setCp(Integer cp) {
         this.cp = cp;
     }
 
@@ -47,12 +47,12 @@ public class Localidad {
         this.provincia = provincia;
     }
 
-    public Localidad poblacion(String poblacion) {
-        setpoblacion(poblacion);
+    public Localidad ciudad(String ciudad) {
+        setCiudad(ciudad);
         return this;
     }
 
-    public Localidad cp(int cp) {
+    public Localidad cp(Integer cp) {
         setCp(cp);
         return this;
     }
@@ -70,22 +70,30 @@ public class Localidad {
             return false;
         }
         Localidad localidad = (Localidad) o;
-        return Objects.equals(poblacion, localidad.poblacion) && cp == localidad.cp && Objects.equals(provincia, localidad.provincia);
+        return Objects.equals(ciudad, localidad.ciudad) && 
+            Objects.equals(cp, localidad.cp) && 
+            Objects.equals(provincia, localidad.provincia);
+        /*
+        // alternativa al return de encima (son equivalentes)
+        boolean salida = true;
+        if (this.ciudad.compareTo(localidad.getCiudad())!=0) salida = false;
+        if (this.provincia.compareTo(localidad.getProvincia())!=0) salida = false;
+        if (this.cp != localidad.getCp()) salida = false;
+        return salida;
+        */
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(poblacion, cp, provincia);
+        return Objects.hash(ciudad, cp, provincia);
     }
 
     @Override
     public String toString() {
         return "{" +
-            " poblacion='" + getpoblacion() + "'" +
+            " ciudad='" + getCiudad() + "'" +
             ", cp='" + getCp() + "'" +
             ", provincia='" + getProvincia() + "'" +
             "}";
     }
-
-
 }
